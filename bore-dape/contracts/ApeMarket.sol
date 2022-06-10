444c5e2fa631f97c1f20b7ae11c146be0796e417
Destiny Aigbe <75917333+Destiny-01@users.noreply.github.com>
2022-06-10 14:59:40 +0100

    Update ApeMarket.sol

--- bore-dape/contracts/ApeMarket.sol
+++ bore-dape/contracts/ApeMarket.sol
@@ -5,12 +5,10 @@ import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
 
 contract ApeMarket {
 	struct Ape {
-		address payable seller;
-        address payable owner;
+        	address owner;
 		address token;
 		uint tokenId;
 		uint price;
-        bool sold;
 	}
 
 	uint private apeId = 0;
@@ -18,25 +16,22 @@ contract ApeMarket {
 
 	function createApe(address token, uint tokenId, uint price) external {
 		Ape memory ape = Ape(
-			payable(msg.sender),
-            payable(msg.sender),
+            		msg.sender,
 			token,
 			tokenId,
-			price,
-            false
+			price
 		);
 		apes[apeId] = ape;
 		apeId++;
-
 	}
 
 	function getApe(uint _apeId) public view returns (Ape memory) {
 		return apes[_apeId];
 	}
 
-    function getApeLength()public view returns (uint){
-        return apeId;
-    }
+	function getApeLength()public view returns (uint){
+		return apeId;
+	}
 
 	function buyApe(uint _apeId) public payable {
 		Ape storage ape = apes[_apeId];
@@ -44,11 +39,8 @@ contract ApeMarket {
 		require(msg.value == ape.price, "Insufficient payment");
 
 		IERC721(ape.token).transferFrom(ape.owner, msg.sender, ape.tokenId);
-		payable(ape.seller).transfer(msg.value);
+		payable(ape.owner).transfer(msg.value);
 
-        ape.sold = true;
-        ape.owner = payable(msg.sender);
-		ape.seller = payable(msg.sender);
-
+        	ape.owner = msg.sender;
 	}
 }
